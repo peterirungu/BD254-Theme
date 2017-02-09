930650cf3fbcfaf9e21d4714c62acf512fc6de1a
peterirungu <peterirungu16@gmail.com>
2017-02-09 19:34:15 +0300

    added other static pages to the theme.

--- pages/home.php
+++ pages/home.php
@@ -8,11 +8,39 @@
 <section>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <h1 class="section-heading">Section Heading</h1>
-                <p class="lead section-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
+            <div class="col-lg-4">
+                <h3><span class="glyphicon glyphicon-list"></span> Blood Tips</h3>
+                
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
             </div>
+            <div class="col-lg-4">
+                <h3><span class="glyphicon glyphicon-globe"></span> Blood Donation Centres in your City</h3>
+
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h3><span class="glyphicon glyphicon-globe"></span> Benefits of becoming a Registered Blood Donor</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
+            </div>       
         </div>
     </div>
 </section>
@@ -25,11 +53,39 @@
 <section>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <h1 class="section-heading">Section Heading</h1>
-                <p class="lead section-lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                <p class="section-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
+            <div class="col-lg-3">
+                <h3 class="text-center">Blogs </h3>
+                
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
             </div>
+            <div class="col-lg-6">
+                <h3 class="text-center"></span> Blood Donors Network</h3>
+
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h3 class="text-center"></span> Upcoming Events</h3>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
+            </div>  
         </div>
         <!-- /.row -->
     </div>
