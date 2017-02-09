34e8d905f26ed95f1ca42a555d7d4f9f26a42e02
peterirungu <peterirungu16@gmail.com>
2017-02-09 15:38:13 +0300

    styling the navigation bar

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; BloodDonation 2017</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container -->
+    </footer>
+
+    <!-- jQuery -->
+    <script src="assets/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+include 'header.php'; 
+include 'navigation.php';
+include 'pages/home.php';
+include 'footer.php';   
+?>
