ed9086987d2f21a802b9101f9916085c1cde1f77
peterirungu <peterirungu16@gmail.com>
2017-02-10 10:39:07 +0300

    added images

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,4 @@
 <!-- Full Width Image Header with Logo -->
-<!-- Image backgrounds are set within the full-width-pics.css file. -->
 <header class="image-bg-fluid-height">
     <img class="img-responsive img-center" src="http://placehold.it/200x200&text=Logo" alt="">
 </header>
@@ -9,7 +8,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4">
-                <h3><span class="glyphicon glyphicon-list"></span> Blood Tips</h3>
+                <h3 class="bdh3"><span class="glyphicon glyphicon-list"></span> Blood Tips</h3>
                 
                 <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -20,24 +19,22 @@
                 <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h3><span class="glyphicon glyphicon-globe"></span> Blood Donation Centres in your City</h3>
+                <h3 class="bdh3"><span class="glyphicon glyphicon-globe"></span> Blood Donation Centres in your City</h3>
 
                 <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
+                consequat. Duis aute irure dolor in.</p>
 
                 <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h3><span class="glyphicon glyphicon-globe"></span> Benefits of becoming a Registered Blood Donor</h3>
+                <h3 class="bdh3"><span class="glyphicon glyphicon-check"></span> Benefits of becoming a Registered Blood Donor</h3>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
+                consequat. Duis aute irure dolor in </p>
 
                 <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
             </div>       
@@ -46,7 +43,6 @@
 </section>
 
 <!-- Fixed Height Image Aside -->
-<!-- Image backgrounds are set within the full-width-pics.css file. -->
 <aside class="image-bg-fixed-height"></aside>
 
 <!-- Content Section -->
@@ -54,37 +50,34 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-3">
-                <h3 class="text-center">Blogs </h3>
-                
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
+                <h3 class="text-center bdgreen">Blogs </h3>                
+                <div class="col-lg-12">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
             </div>
             <div class="col-lg-6">
-                <h3 class="text-center"></span> Blood Donors Network</h3>
-
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
+                <h3 class="text-center bdcolor"></span> Blood Donors Network</h3>                
+                <div class="col-lg-12">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
             </div>
             <div class="col-lg-3">
-                <h3 class="text-center"></span> Upcoming Events</h3>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="">Read More &raquo;</a></p>
+                <h3 class="text-center bdgreen"></span> Upcoming Events</h3>
+                <div class="col-lg-12">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
             </div>  
         </div>
         <!-- /.row -->
