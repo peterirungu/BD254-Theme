e547fa0007bd932b2b8b5876c12c5a8faba91dec
peterirungu <peterirungu16@gmail.com>
2017-03-20 17:43:42 +0300

    update

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,6 @@
 <!-- Full Width Image Header with Logo -->
 <header class="image-bg-fluid-height">
-    <img class="img-responsive img-center" src="http://placehold.it/200x200&text=Logo" alt="">
+    
 </header>
 
 <!-- Content Section -->
